560435d5f28d9009de6ebb916aa3884a1e6cd3a6
dennisgushue-arch <dennis.gushue@gmail.com>
2026-03-01 11:17:05 -0800

    Add initial Flutter files for the Truce app.

---
+++ lib/firebase_stub.dart
@@ -0,0 +1,9 @@
+// Firebase integration stub
+// Add Firebase dependencies to your pubspec.yaml
+// import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseStub {
+  static Future<void> initialize() async {
+    // Initialize Firebase and setup configurations here.
+  }
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+// Models
+class User {
+  final String name;
+  final String email;
+
+  User({required this.name, required this.email});
+}
